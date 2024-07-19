c79f364e5f41abd565baa8b9a8262e1991c79a80
Saurabh Bisht <saurabhb7678@gmail.com>
2024-07-19 17:59:30 +0530

    development

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -12,86 +12,102 @@ function Navbar() {
     console.log(sidebar);
     sidebar.style.display = "none";
   };
-  return (
-    <>
-      <div className="navbar">
-        <ul className="sidebar">
-          <li onClick={hideSidebar}>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 -960 960 960"
-                width="24px"
-                fill="#5f6368"
-              >
-                <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
-              </svg>
-            </a>
-          </li>
-          <li>
-            <a href="/">
-              <div>Home</div>
-            </a>
-          </li>
-          <li>
-            <a href="/">
-              <div>Home</div>
-            </a>
-          </li>
-          <li>
-            <a href="/">
-              <div>Home</div>
-            </a>
-          </li>
-        </ul>
-        <ul className="horizontalbar">
-          <li>
-            <a href="/">
-              <img
-                id="logo-img"
-                src="src\assets\images\dimesnion-logo-s.png"
-                alt=""
-              />
-            </a>
-          </li>
-          <li className="hide-on-mobile">
-            <a href="/">Home</a>
-          </li>
-          <li className="hide-on-mobile">
-            <a href="/">Home</a>
-          </li>
-          <li className="hide-on-mobile">
-            <a href="/">Home</a>
-          </li>
-          <li className="hide-on-mobile">
-            <a href="/">Home</a>
-          </li>
-          <li className="hide-on-desktop" onClick={showSidebar}>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 -960 960 960"
-                width="24px"
-                fill="#5f6368"
-              >
-                <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
-              </svg>
-            </a>
-          </li>
-        </ul>
 
-        {/* <div id="cta">
-          <a href="">
-            <button>Check KYC</button>
+  window.onload = () => {
+    window.addEventListener("scroll", function () {
+      console.log(scrollY);
+      if (scrollY >= 150) {
+        let navbar = document.getElementsByClassName("navbar");
+        navbar.style.backGroundColor = "grey";
+      }
+    });
+  };
+
+  return (
+    <div className="navbar">
+      <ul className="sidebar">
+        <li onClick={hideSidebar}>
+          <a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#5f6368"
+            >
+              <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+            </svg>
           </a>
-          <a href="">
-            <button>Invest Online</button>
+        </li>
+        <li>
+          <a href="/">
+            <div>Home</div>
           </a>
-        </div> */}
-      </div>
-    </>
+        </li>
+        <li>
+          <a href="/">
+            <div>Service</div>
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <div>Invest</div>
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <div>Investor corner</div>
+          </a>
+        </li>
+      </ul>
+      <ul className="horizontalbar">
+        <li>
+          <a href="/">
+            <img
+              id="logo-img"
+              src="src\assets\images\dimesnion-logo-s.png"
+              alt=""
+            />
+          </a>
+        </li>
+        <li className="hide-on-mobile">
+          <a href="/">Home</a>
+        </li>
+        <li className="hide-on-mobile">
+          <a href="/">Service</a>
+        </li>
+        <li className="hide-on-mobile">
+          <a href="/">Invest</a>
+        </li>
+        <li className="hide-on-mobile">
+          <a href="/">Investor corner</a>
+        </li>
+        <li className="hide-on-mobile">
+          <a href="/">
+            <div className="navBtn">Check KYC</div>
+          </a>
+        </li>
+        <li className="hide-on-mobile">
+          <a href="/">
+            <div className="navBtn">Invest Online</div>
+          </a>
+        </li>
+
+        <li className="hide-on-desktop" onClick={showSidebar}>
+          <a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#5f6368"
+            >
+              <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+            </svg>
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
 
